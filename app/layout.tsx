58bdb79caef179b08dacc2f1aff4479b3a8ada4b
hydrosesam <71249432+hydrosesam@users.noreply.github.com>
2024-01-17 15:56:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+
+  export const dynamic = 'auto'
+// 'auto' | 'force-dynamic' | 'error' | 'force-static'
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
